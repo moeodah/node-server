1e20a65fd3183bff8349544e3800a38fd028cedd
Mohammad Odah <33989036+moeodah@users.noreply.github.com>
2022-09-29 17:18:08 +0300

    Delete src directory

--- src/models/Milage.js
+++
@@ -1,9 +0,0 @@
-module.exports = (sequelize, DataTypes) => {
-    const Milage = sequelize.define('Milage', {
-        name: DataTypes.STRING,
-        milage: DataTypes.STRING,
-        date: DataTypes.STRING,
-        addedBy: DataTypes.STRING,
-    })
-    return Milage
-  }
